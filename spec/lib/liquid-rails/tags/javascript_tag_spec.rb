d7a1bd5062d0041cfef0b38521e7a3283d01bd44
SpaYco <zizou.aziz101@gmail.com>
2024-09-27 00:57:03 +0100

    Add javascript tag

--- spec/lib/liquid-rails/tags/javascript_tag_spec.rb
+++ spec/lib/liquid-rails/tags/javascript_tag_spec.rb
@@ -1,14 +1,14 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# module Liquid
-#   module Rails
-#     describe JavascriptTag, type: :tag do
-#       let(:context) { ::Liquid::Context.new({}, {}, { view: view }) }
+module Liquid
+  module Rails
+    describe JavascriptTag, type: :tag do
+      let(:context) { ::Liquid::Context.new({}, {}, { view: view }) }
 
-#       it '#crsf_meta_tags' do
-#         result = Liquid::Template.parse("{% javascript_tag %} alert('Hello Liquid-Rails!') {% endjavascript_tag %}").render(context)
-#         expect(result).to eq(%|<script type=\"text/javascript\">\n//<![CDATA[\n alert('Hello Liquid-Rails!') \n//]]>\n</script>|)
-#       end
-#     end
-#   end
-# end
+      it '#crsf_meta_tags' do
+        result = Liquid::Template.parse("{% javascript_tag %} alert('Hello Liquid-Rails!') {% endjavascript_tag %}").render(context)
+        expect(result).to eq(%|<script type=\"text/javascript\">\n//<![CDATA[\n alert('Hello Liquid-Rails!') \n//]]>\n</script>|)
+      end
+    end
+  end
+end
